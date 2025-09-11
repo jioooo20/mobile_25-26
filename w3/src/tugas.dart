710ef94ae3d6b7d049353c1171fd8d5815ff627d
jioooo20 <giojio825@gmail.com>
2025-09-11 15:18:57 +0700

    tugas praktikum

---
+++ w3/src/tugas.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  int angka = 0;
+  while (angka <=201){
+    bool isPrima = true;
+    if (angka < 2) {
+      isPrima = false;
+    } else {
+      for (int i = 2; i <= angka ~/ 2; i++) {
+      if (angka % i == 0) {
+        isPrima = false;
+        break;
+      }
+      }
+    }
+    if (isPrima) {
+      print("$angka \nNIM: 2341720096 \nNama: Giovano Alkandri");
+    }
+    angka++;
+  }
+}
