c238933b9aa53628984a5826fc6cde3b012de8c4
jioooo20 <giojio825@gmail.com>
2025-09-18 11:30:55 +0700

    p1

---
+++ w4/src/p1.dart
@@ -0,0 +1,23 @@
+void main() {
+  // var list = [1, 2, 3];
+  // assert(list.length == 3);
+  // assert(list[1] == 2);
+  // print(list.length);
+  // print(list[1]);
+
+  // list[1] = 1;
+  // assert(list[1] == 1);
+  // print(list[1]);
+
+  var list = ['Gio', 2341720096,3, 4, null];
+  assert(list.length == 5);
+  assert(list[1] == 2341720096);
+  print(list.length);
+  print(list[1]);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+  print(list[1]);
+
+
+}
